9c394b820f27c3dac62af50f2797bbee88cfc029
Polina-Myatlik <121228435+Polina-Myatlik@users.noreply.github.com>
2023-04-10 23:25:08 +0300

    Add files via upload

---
+++ Лаба7/Лаба7/Form1.Designer.cs
@@ -0,0 +1,407 @@
+﻿namespace Лаба7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.bPoint = new System.Windows.Forms.Button();
+            this.b0 = new System.Windows.Forms.Button();
+            this.bSin = new System.Windows.Forms.Button();
+            this.bCos = new System.Windows.Forms.Button();
+            this.bT = new System.Windows.Forms.Button();
+            this.bM = new System.Windows.Forms.Button();
+            this.Lnv = new System.Windows.Forms.CheckBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.bCohran = new System.Windows.Forms.Button();
+            this.bOtmena = new System.Windows.Forms.Button();
+            this.bOk = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(38, 230);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(69, 61);
+            this.b1.TabIndex = 4;
+            this.b1.Text = "1";
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.b1_MouseClick);
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(113, 230);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(69, 61);
+            this.b2.TabIndex = 5;
+            this.b2.Text = "2";
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.b2_MouseClick);
+            // 
+            // b3
+            // 
+            this.b3.Location = new System.Drawing.Point(188, 230);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(69, 61);
+            this.b3.TabIndex = 6;
+            this.b3.Text = "3";
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.b3_MouseClick);
+            // 
+            // b4
+            // 
+            this.b4.Location = new System.Drawing.Point(38, 163);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(69, 61);
+            this.b4.TabIndex = 7;
+            this.b4.Text = "4";
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.MouseClick += new System.Windows.Forms.MouseEventHandler(this.b4_MouseClick);
+            // 
+            // b5
+            // 
+            this.b5.Location = new System.Drawing.Point(113, 163);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(69, 61);
+            this.b5.TabIndex = 8;
+            this.b5.Text = "5";
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.MouseClick += new System.Windows.Forms.MouseEventHandler(this.b5_MouseClick);
+            // 
+            // b6
+            // 
+            this.b6.Location = new System.Drawing.Point(188, 163);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(69, 61);
+            this.b6.TabIndex = 9;
+            this.b6.Text = "6";
+            this.b6.UseVisualStyleBackColor = true;
+            this.b6.MouseClick += new System.Windows.Forms.MouseEventHandler(this.b6_MouseClick);
+            // 
+            // b7
+            // 
+            this.b7.Location = new System.Drawing.Point(38, 96);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(69, 61);
+            this.b7.TabIndex = 10;
+            this.b7.Text = "7";
+            this.b7.UseVisualStyleBackColor = true;
+            this.b7.MouseClick += new System.Windows.Forms.MouseEventHandler(this.b7_MouseClick);
+            // 
+            // b8
+            // 
+            this.b8.Location = new System.Drawing.Point(113, 96);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(69, 61);
+            this.b8.TabIndex = 11;
+            this.b8.Text = "8";
+            this.b8.UseVisualStyleBackColor = true;
+            this.b8.MouseClick += new System.Windows.Forms.MouseEventHandler(this.b8_MouseClick);
+            // 
+            // b9
+            // 
+            this.b9.Location = new System.Drawing.Point(188, 96);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(69, 61);
+            this.b9.TabIndex = 12;
+            this.b9.Text = "9";
+            this.b9.UseVisualStyleBackColor = true;
+            this.b9.MouseClick += new System.Windows.Forms.MouseEventHandler(this.b9_MouseClick);
+            // 
+            // bPoint
+            // 
+            this.bPoint.Location = new System.Drawing.Point(188, 297);
+            this.bPoint.Name = "bPoint";
+            this.bPoint.Size = new System.Drawing.Size(69, 61);
+            this.bPoint.TabIndex = 13;
+            this.bPoint.Text = ".";
+            this.bPoint.UseVisualStyleBackColor = true;
+            this.bPoint.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bPoint_MouseClick);
+            // 
+            // b0
+            // 
+            this.b0.Location = new System.Drawing.Point(38, 297);
+            this.b0.Name = "b0";
+            this.b0.Size = new System.Drawing.Size(144, 61);
+            this.b0.TabIndex = 14;
+            this.b0.Text = "0";
+            this.b0.UseVisualStyleBackColor = true;
+            this.b0.MouseClick += new System.Windows.Forms.MouseEventHandler(this.b0_MouseClick);
+            // 
+            // bSin
+            // 
+            this.bSin.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.bSin.Location = new System.Drawing.Point(283, 96);
+            this.bSin.Name = "bSin";
+            this.bSin.Size = new System.Drawing.Size(69, 61);
+            this.bSin.TabIndex = 15;
+            this.bSin.Text = "Sin";
+            this.bSin.UseVisualStyleBackColor = false;
+            this.bSin.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bSin_MouseClick);
+            // 
+            // bCos
+            // 
+            this.bCos.Location = new System.Drawing.Point(283, 163);
+            this.bCos.Name = "bCos";
+            this.bCos.Size = new System.Drawing.Size(69, 61);
+            this.bCos.TabIndex = 16;
+            this.bCos.Text = "Cos";
+            this.bCos.UseVisualStyleBackColor = true;
+            this.bCos.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bCos_MouseClick);
+            // 
+            // bT
+            // 
+            this.bT.Location = new System.Drawing.Point(283, 230);
+            this.bT.Name = "bT";
+            this.bT.Size = new System.Drawing.Size(69, 61);
+            this.bT.TabIndex = 17;
+            this.bT.Text = "Tan";
+            this.bT.UseVisualStyleBackColor = true;
+            this.bT.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bT_MouseClick);
+            // 
+            // bM
+            // 
+            this.bM.Location = new System.Drawing.Point(283, 297);
+            this.bM.Name = "bM";
+            this.bM.Size = new System.Drawing.Size(69, 61);
+            this.bM.TabIndex = 18;
+            this.bM.Text = "/-/";
+            this.bM.UseVisualStyleBackColor = true;
+            this.bM.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bM_MouseClick);
+            // 
+            // Lnv
+            // 
+            this.Lnv.AutoSize = true;
+            this.Lnv.Location = new System.Drawing.Point(283, 35);
+            this.Lnv.Name = "Lnv";
+            this.Lnv.Size = new System.Drawing.Size(54, 24);
+            this.Lnv.TabIndex = 19;
+            this.Lnv.Text = "lnv";
+            this.Lnv.UseVisualStyleBackColor = true;
+            this.Lnv.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Lnv_MouseClick);
+            // 
+            // bCohran
+            // 
+            this.bCohran.Location = new System.Drawing.Point(358, 364);
+            this.bCohran.Name = "bCohran";
+            this.bCohran.Size = new System.Drawing.Size(115, 30);
+            this.bCohran.TabIndex = 22;
+            this.bCohran.Text = "Сохранить...";
+            this.bCohran.UseVisualStyleBackColor = true;
+            this.bCohran.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bCohran_MouseClick);
+            // 
+            // bOtmena
+            // 
+            this.bOtmena.Location = new System.Drawing.Point(540, 364);
+            this.bOtmena.Name = "bOtmena";
+            this.bOtmena.Size = new System.Drawing.Size(80, 30);
+            this.bOtmena.TabIndex = 23;
+            this.bOtmena.Text = "Отмена";
+            this.bOtmena.UseVisualStyleBackColor = true;
+            this.bOtmena.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bOtmena_MouseClick);
+            // 
+            // bOk
+            // 
+            this.bOk.Location = new System.Drawing.Point(479, 364);
+            this.bOk.Name = "bOk";
+            this.bOk.Size = new System.Drawing.Size(55, 30);
+            this.bOk.TabIndex = 24;
+            this.bOk.Text = "ОК";
+            this.bOk.UseVisualStyleBackColor = true;
+            this.bOk.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bOk_MouseClick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(626, 35);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(322, 366);
+            this.panel1.TabIndex = 26;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(954, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(745, 366);
+            this.pictureBox1.TabIndex = 31;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(266, 220);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "1. Введите число\r\n\r\n2. Выберите функцию\r\n\r\n3. Нажмите ОК\r\n\r\n4. Нажмите \"Сохранить" +
+    "...\",\r\nесли хотите сохранить результат\r\n\r\n\r\n\r\n";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(114, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Инструкция";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(38, 35);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(219, 47);
+            this.richTextBox1.TabIndex = 27;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(358, 33);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(262, 47);
+            this.richTextBox2.TabIndex = 28;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(358, 96);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(262, 262);
+            this.richTextBox3.TabIndex = 29;
+            this.richTextBox3.Text = "";
+            this.richTextBox3.DoubleClick += new System.EventHandler(this.richTextBox3_DoubleClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 381);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 30;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1711, 452);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.bOk);
+            this.Controls.Add(this.bOtmena);
+            this.Controls.Add(this.bCohran);
+            this.Controls.Add(this.Lnv);
+            this.Controls.Add(this.bM);
+            this.Controls.Add(this.bT);
+            this.Controls.Add(this.bCos);
+            this.Controls.Add(this.bSin);
+            this.Controls.Add(this.b0);
+            this.Controls.Add(this.bPoint);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button bPoint;
+        private System.Windows.Forms.Button b0;
+        private System.Windows.Forms.Button bSin;
+        private System.Windows.Forms.Button bCos;
+        private System.Windows.Forms.Button bT;
+        private System.Windows.Forms.Button bM;
+        private System.Windows.Forms.CheckBox Lnv;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button bCohran;
+        private System.Windows.Forms.Button bOtmena;
+        private System.Windows.Forms.Button bOk;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ Лаба7/Лаба7/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Лаба7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label3.Text = "";
+            bT.BackColor = Color.Gainsboro;
+            bCos.BackColor = Color.Gainsboro;
+            bSin.BackColor = Color.Gainsboro;
+            timer1.Interval= 500;
+            timer1.Start();
+        }
+        int c = 0;
+        private void Lnv_MouseClick(object sender, MouseEventArgs e)
+        {
+            c++;
+            if(c % 2 != 0)
+            {
+                bSin.Text = "ASin";
+                bCos.Text = "ACos";
+                bT.Text = "ATan";
+            }
+            else
+            {
+                bSin.Text = "Sin";
+                bCos.Text = "Cos";
+                bT.Text = "Tan";
+            }
+        }
+
+        private void bPoint_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (richTextBox1.Text.Contains(",") == false)
+            {
+                if(richTextBox1.Text == "")
+                {
+                    richTextBox1.Text += "0,";
+                }
+                else
+                {
+                    richTextBox1.Text += ",";
+                }
+            }
+        }
+
+        private void b0_MouseClick(object sender, MouseEventArgs e)
+        {
+            richTextBox1.Text += "0";
+        }
+
+        private void b1_MouseClick(object sender, MouseEventArgs e)
+        {
+            richTextBox1.Text += "1";
+        }
+
+        private void b2_MouseClick(object sender, MouseEventArgs e)
+        {
+            richTextBox1.Text += "2";
+        }
+
+        private void b3_MouseClick(object sender, MouseEventArgs e)
+        {
+            richTextBox1.Text += "3";
+        }
+
+        private void b4_MouseClick(object sender, MouseEventArgs e)
+        {
+            richTextBox1.Text += "4";
+        }
+
+        private void b5_MouseClick(object sender, MouseEventArgs e)
+        {
+            richTextBox1.Text += "5";
+        }
+
+        private void b6_MouseClick(object sender, MouseEventArgs e)
+        {
+            richTextBox1.Text += "6";
+        }
+
+        private void b7_MouseClick(object sender, MouseEventArgs e)
+        {
+            richTextBox1.Text += "7";
+        }
+
+        private void b8_MouseClick(object sender, MouseEventArgs e)
+        {
+            richTextBox1.Text += "8";
+        }
+
+        private void b9_MouseClick(object sender, MouseEventArgs e)
+        {
+            richTextBox1.Text += "9";
+        }
+
+        private void bM_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (richTextBox1.Text.Contains("-") == false)
+            {
+                richTextBox1.Text = richTextBox1.Text.Insert(0, "-");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(richTextBox1.Text != "" && richTextBox1.Text.Contains(",") == false)
+            {
+                int a = Convert.ToInt32(richTextBox1.Text);
+                if (a > 1 || a < -1)
+                {
+                    label3.Text = "Введите другое число!";
+                    bOk.Enabled = false;
+                }
+                else
+                {
+                    label3.Text = "";
+                    bOk.Enabled = true;
+                }
+            }
+            else if(richTextBox1.Text == "")
+            {
+                label3.Text = "";
+                bOk.Enabled = true;
+            }
+        }
+
+        private void bOtmena_MouseClick(object sender, MouseEventArgs e)
+        {
+            richTextBox1.Text = "";
+            richTextBox2.Text = "";
+            pictureBox1.Image = null;
+        }
+        double znach;
+        string sznach;
+
+        Pen pen = new Pen(Color.Green, 3f);
+        Point[] points = new Point[1000];
+        private void bSin_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(richTextBox1.Text != "")
+            {
+                bSin.BackColor= Color.AliceBlue;
+                bCos.BackColor= Color.Gainsboro;
+                bT.BackColor = Color.Gainsboro;
+                znach = Convert.ToDouble(richTextBox1.Text);
+                Graphics graphics = pictureBox1.CreateGraphics();
+                if (c % 2 == 0)
+                {
+                    sznach = $"Sin({znach}) = {Math.Sin(znach)}";
+                    for (int i = 0; i < points.Length; i++)
+                    {
+                        points[i] = new Point(i, (int)(Math.Sin((double)i / 10) * 100 + 200));
+                    }
+                }
+                else
+                {
+                    sznach = $"Arcsin({znach}) = {Math.Asin(znach)}";
+                    for (int i = 0; i < points.Length; i++)
+                    {
+                        points[i] = new Point(i, (int)(Math.Asin((double)i / 10) * 100 + 200));
+                    }
+                }
+                graphics.DrawLines(pen, points);
+            }
+        }
+
+        private void bCos_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (richTextBox1.Text != "")
+            {
+                Graphics graphics = pictureBox1.CreateGraphics();
+                bSin.BackColor = Color.Gainsboro;
+                bCos.BackColor = Color.AliceBlue;
+                bT.BackColor = Color.Gainsboro;
+                znach = Convert.ToDouble(richTextBox1.Text);
+                if (c % 2 == 0)
+                {
+                    sznach = $"Cos({znach}) = {Math.Cos(znach)}";
+                    for (int i = 0; i < points.Length; i++)
+                    {
+                        points[i] = new Point(i, (int)(Math.Cos((double)i / 10) * 100 + 200));
+                    }
+                }
+                else
+                {
+                    sznach = $"Arccos({znach}) = {Math.Acos(znach)}";
+                    for (int i = 0; i < points.Length; i++)
+                    {
+                        points[i] = new Point(i, (int)(Math.Acos((double)i / 10) * 100 + 200));
+                    }
+                }
+                graphics.DrawLines(pen, points);
+            }
+        }
+
+        private void bT_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (richTextBox1.Text != "")
+            {
+                Graphics graphics = pictureBox1.CreateGraphics();
+                bSin.BackColor = Color.Gainsboro;
+                bCos.BackColor = Color.Gainsboro;
+                bT.BackColor = Color.AliceBlue;
+                znach = Convert.ToDouble(richTextBox1.Text);
+                if (c % 2 == 0)
+                {
+                    sznach = $"Tan({znach}) = {Math.Tan(znach)}";
+                    for (int i = 0; i < points.Length; i++)
+                    {
+                        points[i] = new Point(i, (int)(Math.Tan((double)i / 10) * 100 + 200));
+                    }
+                }
+                else
+                {
+                    sznach = $"Arctan({znach}) = {Math.Atan(znach)}";
+                    for (int i = 0; i < points.Length; i++)
+                    {
+                        points[i] = new Point(i, (int)(Math.Atan((double)i / 10) * 100 + 200));
+                    }
+                }
+                graphics.DrawLines(pen, points);
+            }
+        }
+
+        private void bOk_MouseClick(object sender, MouseEventArgs e)
+        {
+            richTextBox2.Text = sznach;
+        }
+        string text;
+        private void bCohran_MouseClick(object sender, MouseEventArgs e)
+        {
+            text += $"{sznach}\n";
+            richTextBox3.Text += text;
+            using (FileStream a = new FileStream("C:\\Users\\polin\\OneDrive\\Рабочий стол\\Учёба\\Прога 4сем\\Лаба7\\znach.txt", FileMode.Open))
+            {
+                byte[] array = System.Text.Encoding.Default.GetBytes(text);
+
+                a.Write(array, 0, array.Length);
+            }
+        }
+
+        private void richTextBox3_DoubleClick(object sender, EventArgs e)
+        {
+            richTextBox3.Text = "";
+        }
+    }
+}
