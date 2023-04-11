31962a582ee69e4b2a4fce4affadf8d1b9e15a51
Polina-Myatlik <121228435+Polina-Myatlik@users.noreply.github.com>
2023-04-11 09:58:33 +0300

    Add files via upload

---
+++ Лаба7/Лаба7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Лаба7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Form1 form = new Form1();
+            form.ShowDialog();
+        }
+    }
+}
